c6595d75e2fb14830a919d957e91be5aeafc9785
Alessandro Noiato <noesis@Alessandros-MBP.lan>
2018-01-17 19:24:15 +0000

    added exercise comment

--- virtualFunctions.cpp
+++ virtualFunctions.cpp
@@ -1,3 +1,14 @@
+/*
+ This problem is to get you familiar with virtual functions. Create three classes Person, Professor and Student. The class Person should have data members name and age. The classes Professor and Student should inherit from the class Person.
+
+The class Professor should have two integer members: publications and cur_id. There will be two member functions: getdata and putdata. The function getdata should get the input from the user: the name, age and publications of the professor. The function putdata should print the name, age, publications and the cur_id of the professor.
+
+The class Student should have two data members: marks, which is an array of size  and cur_id. It has two member functions: getdata and putdata. The function getdata should get the input from the user: the name, age, and the marks of the student in  subjects. The function putdata should print the name, age, sum of the marks and the cur_id of the student.
+
+For each object being created of the Professor or the Student class, sequential id's should be assigned to them starting from .
+
+Solve this problem using virtual functions, constructors and static variables. You can create more data members if you want.
+ */
 class Person {
 public:
     string name;
