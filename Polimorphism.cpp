bbb40e465484a0c55f165a322920081497eaac7e
Alessandro Noiato <doctorboredom@hotmail.com>
2018-01-18 19:58:00 +0000

    problem statement

---
+++ Polimorphism.cpp
@@ -0,0 +1,13 @@
+/*Given an abstract base class Cache with member variables and functions: 
+
+mp - Map the key to the node in the linked list
+cp - Capacity
+tail - Double linked list tail pointer
+head - Double linked list head pointer
+set() - Set/insert the value of the key, if present, otherwise add the key as the most recently used key. If the cache has reached its capacity, it should replace the least recently used key with a new key.
+get() - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+
+You have to write a class LRUCache which extends the class Cache and uses the member functions and variables to implement an LRU cache.
+*/
+
+
